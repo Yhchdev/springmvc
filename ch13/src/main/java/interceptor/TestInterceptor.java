a2a6a83547005bd21edbd7e09a544f11bb04e6b6
Yhchdev <630673943@qq.com>
2019-03-03 11:32:58 +0800

    Interceptor 接口方法的执行顺序

---
+++ ch13/src/main/java/interceptor/TestInterceptor.java
@@ -0,0 +1,27 @@
+package interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestInterceptor implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandle方法在控制器处理调用请求方法之前执行");
+        return true;//返回false中断后续操作
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle方法在控制器处理调用请求之后,解析视图之前执行");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion方法在控制器处理请求方法执行完成之后,视图渲染之后执行");
+    }
+}
